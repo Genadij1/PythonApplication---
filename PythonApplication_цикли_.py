1d16db489c1d6321abc8d020d37a4b862b4dbac9
Genacud <bgennadij265@icloud.com>
2023-03-26 12:09:11 +0300

    update

--- PythonApplication_цикли_.py
+++ PythonApplication_цикли_.py
@@ -1,1 +1,15 @@
 
+x = int(input('num_1: '))
+y = int(input('num_2: '))
+for i in range(x, y+1):
+    if i%3 == 0 and i%5 == 0:
+        print("Fizz Buzz")
+        break
+    if i%3 == 0:
+        print('Fizz')
+    if i%5 == 0:
+        print('Buzz')
+   # if i%3 == 0 and i%5 == 0:
+        #print("Fizz Buzz")
+    if i%3 != 0 and i%5 != 0:
+        print(i)
