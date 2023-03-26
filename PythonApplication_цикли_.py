6269d8ebf61e49e606de01b5016050aeed2a3183
Genacud <bgennadij265@icloud.com>
2023-03-26 11:55:10 +0300

    ex_2-done

--- PythonApplication_цикли_.py
+++ PythonApplication_цикли_.py
@@ -1,1 +1,18 @@
 
+x = int(input('num_1: '))
+y = int(input('num_2: '))
+for i in range(x, y+1):
+    print(i)
+print()
+for i in range(y, x-1, -1):
+    print(i)
+print()
+for i in range(x, y+1):
+    if i%7 == 0:
+        print(i)
+print()
+c = 0
+for i in range(x, y+1):
+    if i%5 == 0:
+        c += 1
+        print(c)
