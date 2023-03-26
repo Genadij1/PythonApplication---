223e4238840a6b6689010e955c173435e99cdcde
Genacud <bgennadij265@icloud.com>
2023-03-26 12:35:04 +0300

    ex_3-done

--- PythonApplication_цикли_.py
+++ PythonApplication_цикли_.py
@@ -2,12 +2,11 @@
 x = int(input('num_1: '))
 y = int(input('num_2: '))
 for i in range(x, y+1):
-
-    if i%3 == 0:
-        print('Fizz')
-    if i%5 == 0:
-        print('Buzz')
-    if i%3 == 0 and i%5 == 0:
-        print("Fizz Buzz")
-    if i%3 != 0 and i%5 != 0:
-        print(i)
+     if i%3 == 0 and i%5 == 0:
+           print("Fizz Buzz")
+     elif i%3 == 0:
+           print("Fizz")
+     elif i%5 == 0:
+           print("Buzz")
+     else:
+           print(i)
